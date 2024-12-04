d84f10465435406ea7e87c3eafd06558a9ffe726
Daniel Kunkler <dskunkler@gmail.com>
2024-12-04 00:03:40 -0600

    Day 4

--- 2024/day4/day4.py
+++ 2024/day4/day4.py
@@ -1,31 +1,47 @@
-import re
-
-# def find_and_multiply(st):
-#     return sum(int(a) * int(b) for a, b in re.findall(r'mul\((\d+),(\d+)\)', st))
-
 def pt1():
-    file = open("2024/day4/day4test.txt")
+    file = open("2024/day4/day4.txt")
     line = file.readline()
-
+    wordMap = []
+    while line != '':
+        wordMap.append(line.strip())
+        line = file.readline()
+    sol= 0
+    for row in range(len(wordMap)):
+        for col in range(len(wordMap[0])):
+            word =wordMap[row][col:col+4] 
+            if word == 'XMAS' or word == 'SAMX':
+                sol+=1
+            if row <= len(wordMap) - 4:
+                word =wordMap[row][col] +  wordMap[row+1][col] + wordMap[row+2][col] + wordMap[row+3][col]
+                if word == 'XMAS' or word == 'SAMX':
+                    sol+=1
+                if col  <= len(wordMap[0]) - 4:
+                    word =wordMap[row][col] +  wordMap[row+1][col+1] + wordMap[row+2][col+2] + wordMap[row+3][col+3]
+                    dl =wordMap[row+3][col] +  wordMap[row+2][col+1] + wordMap[row+1][col+2] + wordMap[row][col+3] 
+                    if word == 'XMAS' or word == 'SAMX':
+                        sol +=1
+                    if dl == 'XMAS' or dl == 'SAMX':
+                        sol +=1
+    print(sol)
+                    
 def pt2():
-    print("pt2")
-    # with open("2024/day3/day3.txt") as file:
-    #     line = file.readline()
-
-    # instructions = re.split(r"(don't\(\))|(do\(\))", line)
-    # total = 0
-    # multiply = True
-
-    # for segment in instructions:
-    #     if segment == "don't()":
-    #         multiply = False
-    #     elif segment == "do()":
-    #         multiply = True
-    #     elif segment and multiply:
-    #         total += find_and_multiply(segment)
-
-    # print(total)
+    file = open("2024/day4/day4.txt")
+    line = file.readline()
+    wordMap = []
+    while line != '':
+        wordMap.append(line.strip())
+        line = file.readline()
+    sol= 0
+    for row in range(len(wordMap)):
+        for col in range(len(wordMap[0])):
+            if row <= len(wordMap) - 3 and col  <= len(wordMap[0]) - 3:
+                words = set(['SAM', 'MAS'])
+                word =wordMap[row][col] +  wordMap[row+1][col+1] + wordMap[row+2][col+2] 
+                dl =wordMap[row+2][col] +  wordMap[row+1][col+1] + wordMap[row][col+2]
+                if word in words and dl in words:
+                    sol +=1
+    print(sol)
 
 
 if __name__ == '__main__':
-    pt1()
+    pt2()
