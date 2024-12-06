86873f2d29a1f58d7301219bf7b4b9017b6172eb
Daniel Kunkler <dskunkler@gmail.com>
2024-12-05 22:59:18 -0600

    Day 6 files

---
+++ 2024/day6/day6.py
@@ -0,0 +1,17 @@
+def pt1():
+    file = open("2024/day6/day6test.txt")
+    line = file.readline()
+
+    while line != '\n':
+        line = file.readline()
+
+                    
+def pt2():
+    file = open("2024/day6/day6.txt")
+    line = file.readline()
+
+    while line != '\n':
+        line = file.readline()
+
+if __name__ == '__main__':
+    pt1()
